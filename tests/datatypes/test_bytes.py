e4aaecf6d419b85d40b4a26d0b81646f5fd11417
Javier Candeira <javier@candeira.com>
2016-11-12 22:52:59 +1100

    Add failing tests for Bytes object

--- tests/datatypes/test_bytes.py
+++ tests/datatypes/test_bytes.py
@@ -18,6 +18,77 @@ class BytesTests(TranspileTestCase):
             print('Done.')
             """)
 
+    def test_init(self):
+        self.assertCodeExecution("""
+            x = bytes("Ramón de España", 'utf-8')
+            print(x)
+            """)
+
+        self.assertCodeExecution("""
+            x = bytes("Ramón de España", 'latin-1')
+            print(x)
+            """)
+
+        self.assertCodeExecution("""
+            x = byes("Clive James", 'ascii')
+            print(x)
+            """)
+
+    @expectedFailure
+    def test_init_encode_error(self):
+        # test with accents can't be encoded with ascii,
+        # should raise UnicodeEncodeError
+        self.assertCodeExecution("""
+            x = bytes("Ramón de España", 'ascii')
+            print(x)
+            """)
+
+    def test_equality_properly(self):
+        # testing something against itself not enough for buffer.Buffer
+        # we need two separate values which are equivalent
+        # see http://paste.ubuntu.com/23358563/
+        self.assertCodeExecution("""
+            utf8_ints = bytes([82, 97, 109, 195, 179, 110, 32, 100, 101, 32,
+                               69, 115, 112, 97, 195, 177, 97])
+            utf8_string = bytes("Ramón de España", "utf-8")
+            print(utf8_ints)
+            print(utf8_string)
+            print(utf8_int == utf8_string)
+            """)
+
+    def test_decode(self):
+        self.assertCodeExecution("""
+            print("Encoded with utf-8")
+            x = bytes([82, 97, 109, 195, 179, 110, 32, 100, 101, 32, 69, 115, 112, 97, 195, 177, 97])
+            print(x.decode('utf-8'))
+            """)
+
+        self.assertCodeExecution("""
+            print("Encoded with iso-latin1")
+            x = bytes([82, 97, 109, 243, 110, 32, 100, 101, 32, 69, 115, 112, 97, 241, 97])
+            print(x.decode('latin-1'))
+            """)
+
+        self.assertCodeExecution("""
+            print("Encoded with ascii")
+            x = b'Clive James'
+            print(x.decode('ascii'))
+            """)
+
+    @expectedFailure
+    def test_decode_error(self):
+        self.assertCodeExecution("""
+            # encoded with utf_8
+            x = bytes([82, 97, 109, 195, 179, 110, 32, 100, 101, 32, 69, 115, 112, 97, 195, 177, 97])
+            print(x.decode('ascii'))
+            """)
+
+        self.assertCodeExecution("""
+            // encoded with latin_1
+            x = bytes([82, 97, 109, 243, 110, 32, 100, 101, 32, 69, 115, 112, 97, 241, 97])
+            print(x.decode('utf-8'))
+            """)
+
 
 class UnaryBytesOperationTests(UnaryOperationTestCase, TranspileTestCase):
     data_type = 'bytes'
